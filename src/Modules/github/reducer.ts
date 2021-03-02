f2a98e9e800a3d99eefcdef808eeb83590432b0a
keemtj <keemgreat@gmail.com>
2021-03-02 10:26:57 +0900

    :recycle: Add reducerUtils.ts to lib dir

--- src/Modules/github/reducer.ts
+++ src/Modules/github/reducer.ts
@@ -1,40 +1,47 @@
 import { createReducer } from 'typesafe-actions';
+import { asyncState } from '../../lib/reducerUtils';
 import { GET_USER_PROFILE, GET_USER_PROFILE_ERROR, GET_USER_PROFILE_SUCCESS } from './actions';
 import { GithubAction, GithubState } from './types';
 
 // initialState
 const initialState: GithubState = {
-  userProfile: {
-    loading: false,
-    error: null,
-    data: null,
-  },
+  // userProfile: {
+  //   loading: false,
+  //   error: null,
+  //   data: null,
+  // },
+  userProfile: asyncState.initial(),
 };
 
 const github = createReducer<GithubState, GithubAction>(initialState, {
   [GET_USER_PROFILE]: (state) => ({
     ...state,
-    userProfile: {
-      loading: true,
-      error: null,
-      data: null
-    }
+    // userProfile: {
+    //   loading: true,
+    //   error: null,
+    //   data: null
+    // },
+    userProfile: asyncState.load(),
+    // 기존 상태를 유지하고 싶을 때
+    // userProfile: asyncState.load(state.userProfile.data)
   }),
   [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
     ...state,
-    userProfile: {
-      loading: false,
-      error: null,
-      data: action.payload,
-    }
+    // userProfile: {
+    //   loading: false,
+    //   error: null,
+    //   data: action.payload,
+    // },
+    userProfile: asyncState.success(action.payload),
   }),
   [GET_USER_PROFILE_ERROR]: (state, action) => ({
     ...state,
-    userProfile: {
-      loading: false,
-      error: action.payload,
-      data: null,
-    }
+    // userProfile: {
+    //   loading: false,
+    //   error: action.payload,
+    //   data: null,
+    // },
+    userProfile: asyncState.error(action.payload),
   }),
 });
 
--- src/Modules/github/types.ts
+++ src/Modules/github/types.ts
@@ -1,12 +1,14 @@
 import { ActionType } from 'typesafe-actions';
 import * as actions from './actions';
 import { GithubProfile } from '../../Api/github';
+import { AsyncState } from '../../lib/reducerUtils';
 
 export type GithubAction = ActionType<typeof actions>;
 export type GithubState = {
-  userProfile: {
-    loading: boolean;
-    error: Error | null;
-    data: GithubProfile | null;
-  };
+  // userProfile: {
+  //   loading: boolean;
+  //   error: Error | null;
+  //   data: GithubProfile | null;
+  // };
+  userProfile: AsyncState<GithubProfile, Error>;
 };
